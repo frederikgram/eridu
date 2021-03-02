ad7697b470cbf9f0995aa2b941172fba9c4f169e
frederikgram <frederikxyz@hotmail.com>
2021-03-02 18:12:06 +0100

    Lexer without tokentype identification

---
+++ README.md
@@ -0,0 +1,18 @@
+# Eridu
+My first _full_ attempt at creating a compiled language.  As this
+is the first language that I'm intending to fully implement, I do not intend for this to be complex
+nor super impressive.
+
+## (F)lex, Yacc, Bison ...
+I'm not quite sure yet, whether or not to make a "first implementation" using
+tooling, and then try and work from those, or if I should try and create the entire
+codebase using from-scratch code. 
+
+Using tools seems to be a better way of learning the ecosystem of Language Design,
+however, I'd be cooler to say I made everything myself.
+
+I should definitely write the Lexer myself, as trying my hand at writing a Regex
+based lexer instead of my usual finite-state-machine lexer would be neat, as I write
+a lot of lexers, and it would speed up future projects quite a lot.
+
+## Grammar (BNF)
---
+++ src/lexer.c
@@ -0,0 +1,247 @@
+#include <stdio.h>                                                              
+#include <stdlib.h>                                                             
+#include <ctype.h>                                                              
+#include <string.h>                                                             
+#include <stdarg.h>
+
+
+#define CHARACTER_BUFFER_SIZE   1024
+#define ARRAY_OF_TOKENS_SIZE   102400
+
+/* Token Utils */
+enum TokenType {
+    LITERAL = 0,
+    OPERATOR = 1,
+    IDENTIFIER = 2,
+};
+
+typedef struct Token {
+    
+    char * value;
+    int length;
+    int start;
+    int end;
+    enum TokenType tokentype;
+
+} Token;
+
+/* Lexer Utils */
+typedef struct LexerStatus {
+    char last_char;
+    char current_char;
+    char next_char;
+    int cursor;
+    int is_inside_comment;
+    int is_inside_string;
+
+    char buffer[CHARACTER_BUFFER_SIZE];
+    int buffer_len;
+
+    struct Token tokens[ARRAY_OF_TOKENS_SIZE];
+    int tokens_len;
+
+} LexerStatus;
+
+
+/* Reserved Keywords, operators & seperator definitions */
+const char * const seperators[] = {"(", ")", "{", "}", ";", "[", "]"};
+const int num_seperators = 7;
+
+
+int is_seperator(char c) {
+    
+    for (int i = 0; i < num_seperators; i++) {
+        if (c == seperators[i][0]) {
+            return 1;
+        } 
+    }
+
+    return 0;
+}
+
+// @TODO this needs to be not shit
+int is_single_char_operator(char c) {
+
+    switch(c) {
+        case '+':
+        case '-':
+        case '/':
+        case '*':
+        case '=':
+        case '<':
+        case '>':
+        case '^':
+            return 1;
+        default:
+            return 0;
+    }
+}
+
+// @TODO this needs to be not shit
+int is_dual_char_operator(LexerStatus * status) {
+
+    if (is_single_char_operator(status->current_char) && status->next_char == '=') {
+        return 1;
+    }
+    
+    return 0;
+
+}
+
+void _increment(LexerStatus * status, FILE * ptr) {
+
+        status->last_char    = status->current_char;
+        status->current_char = status->next_char;
+        status->next_char    = fgetc(ptr);
+        status->cursor++;
+}
+
+
+void _build_token_from_buffer(LexerStatus * status) {
+
+    if (status->buffer_len == 0){
+        return;
+    }
+
+    struct Token token;
+    token.value = malloc(sizeof(char) * (status->buffer_len + 1));
+    strncpy(token.value, status->buffer, status->buffer_len);
+    token.length = status->buffer_len;
+    token.start = status->cursor - status->buffer_len + 1;
+    token.end = status->cursor;
+
+    // Reset Character Buffer
+    status->buffer_len = 0;
+    status->tokens[status->tokens_len] = token;
+    status->tokens_len++;
+
+}
+
+void _add_current_char_to_buffer(LexerStatus * status) {
+
+        // If we're not inside a comment block, we can safely add the current
+        // character to our character buffer, to be used next time we build a token.
+        status->buffer[status->buffer_len] = status->current_char;
+        status->buffer_len++;
+}
+
+void main(int argc, char *argv[]){
+
+    // Load input file
+    FILE * inp_fptr;
+    inp_fptr = fopen(argv[1], "r");
+    if (inp_fptr == NULL){
+        fprintf(stderr, "Could not open file: '%s'\n", argv[1]);
+    }
+
+    // Initialize Tracker
+    struct LexerStatus status;
+    status.last_char = '\0';
+    status.current_char = fgetc(inp_fptr);
+    status.next_char    = fgetc(inp_fptr);
+
+    status.cursor            = 0;
+    status.is_inside_comment = 0;
+    status.is_inside_string  = 0;
+    status.buffer_len        = 0;
+    status.tokens_len        = 0;
+
+    char c;
+
+    while (status.current_char != EOF){
+            
+        /* Handle Comment blocks */
+        if (!status.is_inside_comment) {
+        
+            // We're now inside a comment block
+            if (status.current_char == '/' && status.next_char == '*') {
+                status.is_inside_comment = 1; 
+                _increment(&status, inp_fptr);
+                continue;
+            }
+
+        } else {
+       
+            // We've now espaced the comment block 
+            if (status.last_char == '*' && status.current_char == '/') {
+                status.is_inside_comment = 0;
+            }
+            
+            _increment(&status, inp_fptr);
+            continue;
+        }
+
+        /* Token Detection */
+        if (isspace(status.current_char) && !status.is_inside_string && !status.is_inside_comment) {
+        
+            // Assume that our buffer represents a token
+            // and try to build it
+            if (status.buffer_len > 0) {
+                _build_token_from_buffer(&status);        
+            }
+
+            _increment(&status, inp_fptr);
+            continue;
+        }
+
+        // We've exited a string
+        if (status.is_inside_string && status.last_char != '\\' && status.current_char == '\"' && !status.is_inside_comment) {
+            status.is_inside_string = 0;
+            _add_current_char_to_buffer(&status);
+            _build_token_from_buffer(&status);
+            _increment(&status, inp_fptr);
+            continue;
+
+        // We've entered a string
+        } else if (!status.is_inside_string && status.last_char != '\\' && status.current_char == '\"' && !status.is_inside_comment){
+            status.is_inside_string = 1;
+            _build_token_from_buffer(&status);
+            _add_current_char_to_buffer(&status);
+            _increment(&status, inp_fptr);
+            continue;
+        }
+        
+        // Check if the current token is a seperator
+        // if it is, we convert both the current buffer and it
+        // into two seperate tokens, then increment the lexer status.
+        if (is_seperator(status.current_char) && !status.is_inside_string && !status.is_inside_comment) {
+            _build_token_from_buffer(&status);
+            _add_current_char_to_buffer(&status);
+            _build_token_from_buffer(&status);
+            _increment(&status, inp_fptr);
+            continue;
+        }
+
+        // If the current char could combine with the next char
+        // to create a two-character operator such as ==, handle it.
+        if (is_dual_char_operator(&status) && !status.is_inside_string && !status.is_inside_comment){
+            _build_token_from_buffer(&status);
+            _add_current_char_to_buffer(&status);
+            _increment(&status, inp_fptr);
+            _add_current_char_to_buffer(&status);
+            _build_token_from_buffer(&status);
+            _increment(&status, inp_fptr);
+            continue;
+        
+        }
+
+        // This could be combined with the is_seperator check
+        // but this feels more explicit and readable
+        if (is_single_char_operator(status.current_char) && !status.is_inside_string && !status.is_inside_comment) {
+            _build_token_from_buffer(&status);
+            _add_current_char_to_buffer(&status);
+            _build_token_from_buffer(&status);
+            _increment(&status, inp_fptr);
+            continue;
+        
+        }
+
+       _add_current_char_to_buffer(&status); 
+       _increment(&status, inp_fptr);
+    }
+
+    for (int i = 0; i < status.tokens_len; i++) {
+        printf("value\t%s\tstart\t%d\tend\t%d\n", status.tokens[i].value, status.tokens[i].start, status.tokens[i].end);
+    }
+    
+}
